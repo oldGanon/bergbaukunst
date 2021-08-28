89db548f151d5d47b42013a365bd252dc8a434e9
oldGanon <oldGanon@googlemail.com>
2021-08-28 21:36:41 +0200

    fast SIMD triangle rasterizer

--- code/draw.c
+++ code/draw.c
@@ -462,6 +462,8 @@ void Draw_String(bitmap Target, const bitmap Font, color Color, i32 X, i32 Y, co
 //     struct tri_grad l, r;
 // };
 
+#if defined(USE_SCANLINE_RASTERIZER)
+
 inline void Draw__TriangleTexturedShadedVerts3DInternal(bitmap Target, const bitmap Texture, 
                                                         f32 y0, f32 y1, 
                                                         f32 x0, f32 x1,f32 dx0_dy, f32 dx1_dy, 
@@ -644,6 +646,191 @@ inline void Draw__TriangleTexturedShadedVerts3D(bitmap Target, const bitmap Text
     }
 }
 
+#else
+
+void Draw__TriangleTexturedShadedVerts3D(bitmap Target, const bitmap Texture, vertex A, vertex B, vertex C)
+{
+#define PACK_WIDTH_SHIFT 2
+#define PACK_HEIGHT_SHIFT 0
+#define PACK_WIDTH (1 << PACK_WIDTH_SHIFT)
+#define PACK_HEIGHT (1 << PACK_HEIGHT_SHIFT)
+
+#define SUB_SHIFT 4
+#define SUB_STEP (1 << SUB_SHIFT)
+#define SUB_MASK (SUB_STEP - 1)
+
+    __m128 ColOffset = _mm_setr_ps(0, 1, 2, 3);
+    __m128 RowOffset = _mm_setr_ps(0, 0, 0, 0);
+    __m128 One = _mm_set1_ps(1);
+
+    f32 MinX = 0;
+    f32 MinY = 0;
+    f32 MaxX = (f32)Target.Width;
+    f32 MaxY = (f32)Target.Height;
+
+    {
+        // use fixed-point only for X and Y. Avoid work for Z and W.
+        f32 X[3] = { A.Position.x, B.Position.x, C.Position.x };
+        f32 Y[3] = { A.Position.y, B.Position.y, C.Position.y };
+        
+        f32 z[3] = { 1.0f / A.Position.z, 1.0f / B.Position.z, 1.0f / C.Position.z };
+        f32 a[3] = { z[0], 0, 0 };
+        f32 b[3] = { 0, z[1], 0 };
+        f32 s[3] = { A.Shadow, B.Shadow, C.Shadow };
+        f32 u[3] = { A.TexCoord.u, B.TexCoord.u, C.TexCoord.u };
+        f32 v[3] = { A.TexCoord.v, B.TexCoord.v, C.TexCoord.v };
+
+        // Fab(x, y) =     Ax       +       By     +      C              = 0
+        // Fab(x, y) = (ya - yb)x   +   (xb - xa)y + (xa * yb - xb * ya) = 0
+
+        // Compute A = (ya - yb) for the 3 line segments that make up each triangle
+        f32 F12_dx = Y[1] - Y[2];
+        f32 F20_dx = Y[2] - Y[0];
+        f32 F01_dx = Y[0] - Y[1];
+
+        // Compute B = (xb - xa) for the 3 line segments that make up each triangle
+        f32 F12_dy = X[2] - X[1];
+        f32 F20_dy = X[0] - X[2];
+        f32 F01_dy = X[1] - X[0];
+
+        // Compute C = (xa * yb - xb * ya) for the 3 line segments that make up each triangle
+        f32 C0 = X[1] * Y[2] - X[2] * Y[1];
+        f32 C1 = X[2] * Y[0] - X[0] * Y[2];
+        f32 C2 = X[0] * Y[1] - X[1] * Y[0];
+
+        // Compute triangle area
+        f32 invTriArea = 1.0f / (F01_dy * F20_dx - F20_dy * F01_dx);
+
+        z[1] = (z[1] - z[0]) * invTriArea;
+        z[2] = (z[2] - z[0]) * invTriArea;
+        a[1] = (a[1] - a[0]) * invTriArea;
+        a[2] = (a[2] - a[0]) * invTriArea;
+        b[1] = (b[1] - b[0]) * invTriArea;
+        b[2] = (b[2] - b[0]) * invTriArea;
+
+        // Use bounding box traversal strategy to determine which pixels to rasterize
+        MinX = Max(Floor(Min(Min(X[0], X[1]), X[2]) / PACK_WIDTH) * PACK_WIDTH, MinX);
+        MinY = Max(Floor(Min(Min(Y[0], Y[1]), Y[2]) / PACK_HEIGHT) * PACK_HEIGHT, MinY);
+        MaxX = Min(Ceil(Max(Max(X[0], X[1]), X[2])), MaxX);
+        MaxY = Min(Ceil(Max(Max(Y[0], Y[1]), Y[2])), MaxY);
+
+        // Specific to single triangle code follows
+        {
+            __m128 z4[3] = { _mm_set1_ps(z[0]), _mm_set1_ps(z[1]), _mm_set1_ps(z[2]) };
+            __m128 a4[3] = { _mm_set1_ps(a[0]), _mm_set1_ps(a[1]), _mm_set1_ps(a[2]) };
+            __m128 b4[3] = { _mm_set1_ps(b[0]), _mm_set1_ps(b[1]), _mm_set1_ps(b[2]) };
+            __m128 s4[3] = { _mm_set1_ps(s[0]), _mm_set1_ps(s[1]), _mm_set1_ps(s[2]) };
+            __m128 u4[3] = { _mm_set1_ps(u[0]), _mm_set1_ps(u[1]), _mm_set1_ps(u[2]) };
+            __m128 v4[3] = { _mm_set1_ps(v[0]), _mm_set1_ps(v[1]), _mm_set1_ps(v[2]) };
+        
+            // Incrementally compute Fab(x, y) for all the pixels inside the bounding box formed by (startX, endX) and (startY, endY) 
+            __m128 F12_dx4 = _mm_set1_ps(F12_dx);
+            __m128 F20_dx4 = _mm_set1_ps(F20_dx);
+            __m128 F01_dx4 = _mm_set1_ps(F01_dx);
+
+            __m128 F12_dy4 = _mm_set1_ps(F12_dy);
+            __m128 F20_dy4 = _mm_set1_ps(F20_dy);
+            __m128 F01_dy4 = _mm_set1_ps(F01_dy);
+
+            __m128 Col = _mm_add_ps(ColOffset, _mm_set1_ps(MinX));
+            __m128 A12_0 = _mm_mul_ps(F12_dx4, Col);
+            __m128 A20_0 = _mm_mul_ps(F20_dx4, Col);
+            __m128 A01_0 = _mm_mul_ps(F01_dx4, Col);
+
+            __m128 Row = _mm_add_ps(RowOffset, _mm_set1_ps(MinY));
+            __m128 B12_0 = _mm_mul_ps(F12_dy4, Row);
+            __m128 B20_0 = _mm_mul_ps(F20_dy4, Row);
+            __m128 B01_0 = _mm_mul_ps(F01_dy4, Row);
+
+            __m128 F12_Row = _mm_add_ps(_mm_add_ps(A12_0, B12_0), _mm_set1_ps(C0));
+            __m128 F20_Row = _mm_add_ps(_mm_add_ps(A20_0, B20_0), _mm_set1_ps(C1));
+            __m128 F01_Row = _mm_add_ps(_mm_add_ps(A01_0, B01_0), _mm_set1_ps(C2));
+
+            F12_dx4 = _mm_mul_ps(F12_dx4, _mm_set1_ps(PACK_WIDTH));
+            F20_dx4 = _mm_mul_ps(F20_dx4, _mm_set1_ps(PACK_WIDTH));
+            F01_dx4 = _mm_mul_ps(F01_dx4, _mm_set1_ps(PACK_WIDTH));
+
+            F12_dy4 = _mm_mul_ps(F12_dy4, _mm_set1_ps(PACK_HEIGHT));
+            F20_dy4 = _mm_mul_ps(F20_dy4, _mm_set1_ps(PACK_HEIGHT));
+            F01_dy4 = _mm_mul_ps(F01_dy4, _mm_set1_ps(PACK_HEIGHT));
+
+            __m128 zz_dx = _mm_add_ps(_mm_mul_ps(F20_dx4, z4[1]), _mm_mul_ps(F01_dx4, z4[2]));
+            __m128 aa_dx = _mm_add_ps(_mm_mul_ps(F20_dx4, a4[1]), _mm_mul_ps(F01_dx4, a4[2]));
+            __m128 bb_dx = _mm_add_ps(_mm_mul_ps(F20_dx4, b4[1]), _mm_mul_ps(F01_dx4, b4[2]));
+
+            i32 iMinX = Float_toInt(MinX);
+            i32 iMinY = Float_toInt(MinY);
+            i32 iMaxX = Float_toInt(MaxX);
+            i32 iMaxY = Float_toInt(MaxY);
+
+            i32 RowIndex = iMinY * Target.Pitch + iMinX;
+
+            for(i32 y = iMinY; y < iMaxY; y += PACK_HEIGHT)
+            {
+                // Compute barycentric coordinates 
+                i32 Index = RowIndex;
+                __m128 Alpha = F12_Row;
+                __m128 Beta  = F20_Row;
+                __m128 Gamma = F01_Row;
+
+                // Compute barycentric-interpolated attributes
+                __m128 zz = _mm_add_ps(_mm_add_ps(z4[0], _mm_mul_ps(Beta, z4[1])), _mm_mul_ps(Gamma, z4[2]));
+                __m128 aa = _mm_add_ps(_mm_add_ps(a4[0], _mm_mul_ps(Beta, a4[1])), _mm_mul_ps(Gamma, a4[2]));
+                __m128 bb = _mm_add_ps(_mm_add_ps(b4[0], _mm_mul_ps(Beta, b4[1])), _mm_mul_ps(Gamma, b4[2]));
+
+                for(i32 x = iMinX; x < iMaxX; x += PACK_WIDTH)
+                {
+                    // Test Pixel inside triangle
+                    __m128i Mask = _mm_srai_epi32(_mm_castps_si128(_mm_or_ps(_mm_or_ps(Alpha, Beta), Gamma)), 32);
+                    if (!_mm_test_all_ones(Mask))
+                    {
+                        __m128 zzz = _mm_div_ps(One, zz);
+                        __m128 aaa = _mm_mul_ps(aa, zzz);
+                        __m128 bbb = _mm_mul_ps(bb, zzz);
+                        __m128 ccc = _mm_sub_ps(One, _mm_add_ps(aaa, bbb));
+
+                        __m128 uuu = _mm_add_ps(_mm_add_ps(_mm_mul_ps(u4[0], aaa), _mm_mul_ps(u4[1], bbb)), _mm_mul_ps(u4[2], ccc));
+                        __m128 vvv = _mm_add_ps(_mm_add_ps(_mm_mul_ps(v4[0], aaa), _mm_mul_ps(v4[1], bbb)), _mm_mul_ps(v4[2], ccc));
+                        __m128 sss = _mm_add_ps(_mm_add_ps(_mm_mul_ps(s4[0], aaa), _mm_mul_ps(s4[1], bbb)), _mm_mul_ps(s4[2], ccc));
+
+                        f32 U[4], V[4], S[4];
+                        _mm_store_ps(U, uuu);
+                        _mm_store_ps(V, vvv);
+                        _mm_store_ps(S, sss);
+                        u32 M[4];
+                        _mm_storeu_epi32(M, Mask);
+                        color *Dst = Target.Pixels + Index;
+                        for (i32 p = 0; p < 4; ++p)
+                        {
+                            if (M[p]) continue;
+                            const i32 iu = Floor_toInt(U[p]) & 255;
+                            const i32 iv = Floor_toInt(V[p]) & 255;
+                            const u8 is = Floor_toInt(S[p] * 15.999f) & 15;
+                            color Color = *(Texture.Pixels + Texture.Pitch * iv + iu);
+                            if (Color.Value) Dst[p].Value = Color.Value | is;
+                        }
+                    }
+
+                    Index += 4;
+                    Alpha = _mm_add_ps(Alpha, F12_dx4);
+                    Beta  = _mm_add_ps(Beta,  F20_dx4);
+                    Gamma = _mm_add_ps(Gamma, F01_dx4);
+                    zz = _mm_add_ps(zz, zz_dx);
+                    aa = _mm_add_ps(aa, aa_dx);
+                    bb = _mm_add_ps(bb, bb_dx);
+                }
+
+                RowIndex += PACK_HEIGHT * Target.Pitch;
+                F12_Row = _mm_add_ps(F12_Row, F12_dy4);
+                F20_Row = _mm_add_ps(F20_Row, F20_dy4);
+                F01_Row = _mm_add_ps(F01_Row, F01_dy4);
+            }
+        }
+    }
+}
+
+#endif
+
 inline vertex Vertex_Lerp(vertex A, vertex B, f32 t)
 {
     A.Position.x = Lerp(A.Position.x, B.Position.x, t);
@@ -657,6 +844,10 @@ inline vertex Vertex_Lerp(vertex A, vertex B, f32 t)
 
 inline u32 Draw__TriangleClipZ(vertex *V)
 {
+#define CAMERA_FAR 128.0f
+    if (Max(Max(V[0].Position.z, V[1].Position.z), V[2].Position.z) > CAMERA_FAR)
+        return 0;
+
 #define CAMERA_NEAR 0.1f
     vertex T;
     u32 Z = ((V[0].Position.z < CAMERA_NEAR) ? 1 : 0) |
@@ -726,6 +917,7 @@ inline bool Draw__PrepareTriangleVerts(const bitmap Target, vertex *A, vertex *B
     // triangle too small
     if (Abs(Floor(MaxX) - Floor(MinX)) < 1.0f) return false;
     if (Abs(Floor(MaxY) - Floor(MinY)) < 1.0f) return false;
+#if 0
 
     // sort vertices vertically
     if (A->Position.y > B->Position.y)
@@ -745,7 +937,7 @@ inline bool Draw__PrepareTriangleVerts(const bitmap Target, vertex *A, vertex *B
             else                               { vertex T = *B; *B = *C; *C = T; }
         }
     }
-
+#endif
     return true;
 }
 
